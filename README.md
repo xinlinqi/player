c6aae912db1ba21624710a0f1a6c7b356008c8e1
xinlinqi8 <xinlinqi8@gmail.com>
2013-07-29 10:09:22 +0800

    更新UI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 player
 ======
 
+修改tracks.txt可以管理音乐列表啦~
+
 一个简单的音乐播放器，支持歌词同步，调整音量，调整播放进度。SoundManage强力驱动。
 demo:http://www.xiaoqian.org/project/player/
--- js/index.js
+++ js/index.js
@@ -1,207 +1,162 @@
-;(function($, win){
-	var $songProgressBar = $('#songProgressBar'), //歌曲进度条面板
-	$songProgressLoad = $('#songProgressLoad'), //歌曲加载进度面板
-	$songProgressCurrent = $('#songProgressCurrent'), //当前歌曲进度条位置
-	$songProgressHideBar = $('#songProgressHideBar'), //歌曲进度条占位面板
-	$volumeProgressCurrent = $('#volumeProgressCurrent'), //当前音量进度条位置
-	$volumeCurrentBtn = $('#volumeCurrentBtn'), //音量游标按钮
-	$volumeProgressBar = $('#volumeProgressBar'), //音量进度条面板
-	$volumePanel = $('#volumePanel'), //音量面板
-	$volumeBtn = $('#volumeBtn'), //音量按钮
-	$playControl = $('#playControl'), //播放控制
-	$playBtn = $('#playBtn'), //播放按钮
-	$pauseBtn = $('#pauseBtn'), //暂停按钮
-	$title = $('#title'), //歌曲名和歌手名
-	$coverImage = $('#coverImage'), //封面图片
-	$lrc = $('#lrc'), //歌词元素
-	$lrcPanel = $('#lrcPanel'), //歌词面板
-	$playList = $('#playList'), //播放列表
-	$playListBtn = $('#playListBtn'), //显示播放列表按钮
-	soundId = 'mySound', //音频ID
-	songTime = 0, //歌曲时间
-	progressWidth = 90, //进度条宽度
-	playlistId = 0, //播放列表ID
-	player = player || {};
-	
-	//soundManager配置
-	soundManager.url = 'swf/';
-	soundManager.debugMode = false;
-
-	player.css = {
-		vs: 'vs', //显隐 visibility: hidden;
-		bd: 'bd', //边框颜色 border-color: #BBB
-		hg: 'hg', //高亮歌词 color: #393c4b
-		cr: 'cr' //高亮播放列表 background-color: #30B192;color: #FFF
-	};
+;(function($){
+	var $songProgressBar = $('#songProgressBar'),//歌曲进度条面板
+	$songProgressCurrent = $('#songProgressCurrent'),//当前歌曲进度条位置
+	$songProgressHideBar = $('#songProgressHideBar'),//歌曲进度条占位面板
+	$volumeProgressCurrent = $('#volumeProgressCurrent'),//当前音量进度条位置
+	$volumeCurrentBtn = $('#volumeCurrentBtn'),//当前音量按钮位置
+	$volumeProgressBar = $('#volumeProgressBar'),//音量进度条面板
+	$volumePanel = $('#volumePanel'),//音量面板
+	$volumeBtn = $('#volumeBtn'),//音量按钮
+	$playBtn = $('#playBtn'),//播放按钮
+	$pauseBtn = $('#pauseBtn'),//暂停按钮
+	$listBtn = $('#list_btn'),//播放列表按钮
+	$lrc = $('#lrc'),//歌词元素
+	$song_title = $('#song_ti'),
+	$song_author = $('#song_ar');
+	host = './tracks/',//歌曲文件路径
+	songTime = 0,//歌曲时间
+	player = {},
+	songList = [],
+	currentSong = 0;
 
 	player.init = function(){
-		this.fn.create();
-		this.fn.createPlayListUI();
-		this.fn.fillData();
-		this.fn.bind();
+		this.create();
+		this.bind();
 	};
 
-	player.fn = {
-		create: function(callback){
-			playlistId = playlistId ? playlistId : 0;
-			
-			if(player.sound){
-				player.sound.destruct(); //销毁音频对象
-				if(playlistId == playlist.length){ //播放列表循环播放
-		            playlistId = 0;
-		        }
-			}
+	player.set = function(time){
+		time = time || 0;
+		songTime = time;
+	}
 
-			//初始化音频对象
-		  	soundManager.onready(function(){
-		  		player.sound = soundManager.createSound({
-			        id: soundId,//音频ID
-			        url: playlist[playlistId].mp3,//播放地址
+	player.create = function(){
+		//初始化音频对象
+		soundManager.setup({
+		  	url: 'swf/',//flash文件
+		  	debugMode: true,//调试模式
+		  	onready: function() {
+			    var mySoundObject = soundManager.createSound({
+			        id: songList[currentSong].sid,//音频ID
+			        url: songList[currentSong].mp3,//播放地址
 			        volume: 50,//音量大小
 			        autoLoad: true,//自动加载
-			        // autoPlay: true,
 			        onload:function(){
-			        	if(this.readyState == 3){
-			        		songTime = Math.floor((this.bytesTotal/this.bytesLoaded)*this.duration);//记录歌曲时间
-			      			callback && callback();
-			        	}else if(this.readyState == 2){
-			        		alert(this.url + ' 音频加载失败');
-			        	}else if(this.readyState == 1){
-			        		alert('正在加载中');
-			        	}else if(this.readyState == 0){
-			        		alert('未初始化');
-			        	}else{
-			        		alert('情况好乱');
-			        	}
-			      	},
-			      	whileloading: function(){
-			      		var songProgressLoad = (this.bytesLoaded / this.bytesTotal) * progressWidth;
-			      		//判断当前进度是否越界
-					    if(songProgressLoad <= progressWidth){
-					    	$songProgressLoad.css('width', songProgressLoad + '%');//设置进度条
-					    }
+			      		songTime = Math.floor((this.bytesTotal/this.bytesLoaded)*this.duration);//记录歌曲时间
+						showBtn($playBtn, $pauseBtn);
+						soundManager.play(songList[currentSong].sid);			      		
 			      	},
 			      	whileplaying: function() {
-					    var songProgressCurrent = (this.position / this.duration) * progressWidth,//当前播放进度
+					    var currentProgressPos = (this.position / this.duration) * 90,//当前播放进度
 					    pos = (this.position / 1000).toFixed();//当前播放时间
 
 					    //判断当前进度是否越界
-					    if(songProgressCurrent >= progressWidth){
+					    if(currentProgressPos >= 90){
 					    	return;
 					    }
-					    $songProgressCurrent.css('width', songProgressCurrent + '%');//设置进度条
+					    $songProgressCurrent.css('width', currentProgressPos + '%');//设置进度条
 
 				    	//判断当前播放时间是否匹配到歌词对象
 						if(player.lrc.options.data[pos]){
 							//判断时间帧，防止多次执行
 							if(pos != player.lrc.options.index){
 								player.lrc.options.index = pos;//记录歌词下标
-								$lrc.animate({'top': '-=24px'},'slow').find('p').removeClass(player.css.hg)
-								.end().find('p[time='+ pos +']').addClass(player.css.hg);//滚动歌词，并高亮当前行歌词
+								$lrc.animate({'top': '-=38px'},'slow').find('p').removeClass('high')
+								.end().find('p[time='+ pos +']').addClass('high');//滚动歌词，并高亮当前行歌词
 							}
 						}
+						document.title =  songList[currentSong].title + "-" + songList[currentSong].artist + " | 一个朴素的音乐播放器";
 				  	},
 				    onfinish: function() {
+					    showBtn($pauseBtn,$playBtn);
 					    $songProgressCurrent.animate({'width':0},'slow');//重置进度条
 					    $lrc.animate({'top':0},'slow');//重置歌词位置
-						
-						++playlistId;
-					    player.fn.create(function(){
-					    	player.sound.play();
-					    });
-					    player.fn.fillData();
+					    soundManager.setPosition(songList[currentSong].sid,0);//重置播放位置
+					    currentSong = (currentSong + 1 <= songList.length - 1) ? currentSong + 1: 0;
+						player.changeSong();
+						player.setSkin();
 					}
 			    });
-		  	});
-		},
-		fillData: function(){
-			$coverImage[0].src = playlist[playlistId].coverImage; //填充封面图片
-			$title.text(playlist[playlistId].songName +' - '+ playlist[playlistId].singerName); //填充歌曲信息
-			player.lrc.getData(playlist[playlistId].lrc); //填充歌词
-			player.fn.highCurrentLyrics(); //高亮当前
-		},
-		createPlayListUI: function(){
-			var html = [];
-			for(var i = 0, len = playlist.length; i < len; i++){
-				html[i] = '<li><span>'+ (i + 1) +'.</span>'+ playlist[i].songName +' - '+ playlist[i].singerName +'</li>';
-			}
-			$playList.html('<ul>' +html.join(' ')+ '</ul>');
-		},
-		highCurrentLyrics:function(){
-			$playList.find('li').eq(playlistId).addClass(player.css.cr).siblings().removeClass(player.css.cr);//高亮当前
-		},
-		bind: function(){
-
-			//播放控制
-			$playControl.click(function(){
-				togglePlay();
-			});
-
-			//打开音量面板
-			$volumeBtn.click(function(){
-				($volumePanel.is(':hidden')) ? $volumePanel.show() : $volumePanel.hide();
-			});
-
-			//播放列表
-			$playList.on('click', 'li', function(){
-				playlistId = $(this).index();
-
-				//播放歌曲
-				player.fn.create(function(){
-					player.sound.play();
-				});
-				player.fn.fillData();
-				
-				//重置元素
-				$songProgressCurrent.animate({'width':0},'slow'); //重置进度条
-				$lrc.animate({'top':0},'slow'); //重置歌词位置
-				togglePlay(true);
-			});
-
-			//显示播放列表
-			$playListBtn.click(function(){
-				$(this).toggleClass(player.css.bd);
-				$lrcPanel.toggleClass(player.css.vs);
-				$playList.toggleClass(player.css.vs);
-			});
-
-			//指定进度条位置开始播放
-			$songProgressHideBar.click(function(event){
-				player.sound.pause();//暂停播放
-				togglePlay(true);//显示按钮
-
-				var pos = player.volume.getAbsPos(this),//获取进度条容器位置
-				page = player.volume.getPageXY(event),//获取鼠标位置
-				songProgressCurrent = ((page.x - pos.x) / $(this).width()) * progressWidth,//当前进度条位置百分比
-				secondWidth = songTime / $(this).width(),//歌曲每秒所占宽度
-				currentPlayTime = (page.x - pos.x) * secondWidth;//当前播放时间
-
-				//设置进度条位置
-				$songProgressCurrent.animate({'width': songProgressCurrent + '%'},'slow',function(){
-					player.sound.setPosition(currentPlayTime);//指定时间播放歌曲
-					player.sound.resume();//恢复歌曲
-					player.sound.play();//播放歌曲
-				});
-
-				player.lrc.scroll(currentPlayTime);//滚动歌词
-			});
-
-			//按下音量游标
-			$volumeCurrentBtn.mousedown(player.volume.down);
-
-			//移动音量游标
-			$(document).mousemove(player.volume.move);
-
-			//放下音量游标
-			$(document).mouseup(player.volume.up);
-		}
+		  	}
+		});
+		player.lrc.getData();
+		player.setSkin();
 	};
 
+	player.bind = function(){
+		//播放歌曲
+		$playBtn.click(function(){
+			showBtn($(this),$pauseBtn);
+			soundManager.play(songList[currentSong].sid);
+		});
+
+		//暂停播放
+		$pauseBtn.click(function(){
+			showBtn($(this),$playBtn);
+			soundManager.pause(songList[currentSong].sid);
+		});
+		
+		//显示/隐藏播放列表
+		$listBtn.click(function(){
+			togglePanel($("#song_lrc"), $("#play_list"));
+		});
+
+		//打开音量面板
+		$volumeBtn.click(function(){
+			($volumePanel.is(':hidden')) ? $volumePanel.show() : $volumePanel.hide();
+		});
+
+		//指定进度条位置开始播放
+		$songProgressHideBar.click(function(event){
+			soundManager.pause(songList[currentSong].sid);//暂停播放
+			showBtn($playBtn,$pauseBtn);
+
+			var pos = player.volume.getAbsPos(this),//获取进度条容器位置
+			page = player.volume.getPageXY(event),//获取鼠标位置
+			currentProgressPos = ((page.x - pos.x) / $(this).width()) * 90,//当前进度条位置百分比
+			secondWidth = songTime / $(this).width(),//歌曲每秒所占宽度
+			currentPlayTime = (page.x - pos.x) * secondWidth;//当前播放时间
+
+			//设置进度条位置
+			$songProgressCurrent.animate({'width': currentProgressPos + '%'},'slow',function(){
+				soundManager.setPosition(songList[currentSong].sid,currentPlayTime);//指定时间播放歌曲
+				soundManager.resume(songList[currentSong].sid);//恢复歌曲
+				soundManager.play(songList[currentSong].sid);//播放歌曲
+			});
+			player.lrc.scroll(currentPlayTime);//滚动歌词
+		});
+
+		//按下音量游标
+		$volumeCurrentBtn.mousedown(player.volume.down);
+
+		//移动音量游标
+		$(document).mousemove(player.volume.move);
+
+		//放下音量游标
+		$(document).mouseup(player.volume.up);
+	};
+	
+	    
+    player.changeSong = function(){
+    	soundManager.reboot();
+    	player.lrc.getData();
+    	songTime = 0;
+		$songProgressCurrent.animate({'width':0}, 'slow');//重置进度条
+	    $lrc.animate({'top':0},'slow');//重置歌词位置
+	    soundManager.setPosition(songList[currentSong].sid, 0);//重置播放位置  
+    };
+	
+	player.setSkin = function(){
+		$(".cover img").attr("src", songList[currentSong].cover);
+		$('#item-' + songList[currentSong].sid).addClass("active");
+		$('#item-' + songList[currentSong].sid).parent().siblings().children().removeClass("active");
+	};
+	
 	//歌词
 	player.lrc = {
 		options:{
-			index: -1, //记录歌词下标
-			data: {} //歌词数据
+			index: -1,//记录歌词下标
+			data: {},//歌词数据
+			html: []//歌词结构
 		},
 		//根据歌曲播放进度滚动歌词
 		scroll:function(time){
@@ -217,43 +172,75 @@
 			if(jumpTime){
 				var $jump = $lrc.find('p[time='+ jumpTime +']'),
 				currentTop = $jump.offset().top - $lrc.offset().top - parseInt($lrc.css('padding-top'));//计算滚动位置
-				$jump.addClass(player.css.hg).siblings().removeClass(player.css.hg);//高亮当前歌词
+				$jump.addClass('high').siblings().removeClass('high');//高亮当前歌词
 				$lrc.animate({'top': -currentTop},'slow');//滚动歌词到指定位置
 			}else{
-				$lrc.find('p').removeClass(player.css.hg);//重置相关元素
+				$lrc.find('p').removeClass('high');//重置相关元素
 				$lrc.animate({'top': 0},'slow');//滚动歌词到原位
 			}
 		},
 		//读取歌词文件
-		getData:function(url){
-			player.lrc.options.data = {};//清空歌词对象
-	        $.get(url, function(data){
-	        	var arr = data.split("\n");//分割每行歌词
-	        	//遍历歌词
-	        	for(var i = 0, len = arr.length; i < len; i++){
-	        	 	var text = arr[i].replace(/\[\d*:\d*((\.|\:)\d*)*\]/g,''),//获取歌词
-	        	 	timeArr = arr[i].match(/\[\d*:\d*((\.|\:)\d*)*\]/g);//获取时间帧
-	        	 	if(timeArr){
-	        	 		//遍历时间帧
-	        	 		for(var j = 0, lenn = timeArr.length; j < lenn; j++){
-	        	 			var min = Math.floor(timeArr[j].match(/\[\d*/i).toString().slice(1)),//分
-	        	 			sec = Math.floor(timeArr[j].match(/\:\d*/i).toString().slice(1)),//秒
-	        	 			time = min * 60 + sec;//转换时间
-	        	 			player.lrc.options.data[time] = $.trim(text);//歌词对象
-	        	 		}
-	        	 	}
-	        	}
-	        	player.lrc.createUI(player.lrc.options.data);//创建歌词结构
-	        });
+		getData:function(){
+			$.get(host + songList[currentSong].title + '.lrc', function(data){
+				var arr = data.split("\n");//分割每行歌词
+				var title = arr[0].replace(']', '').split(':')[1],
+				author = arr[1].replace(']', '').split(':')[1];
+				//遍历歌词
+				player.lrc.options.data = []
+				for(var i = 0, len = arr.length; i < len; i++){
+					var text = arr[i].replace(/\[\d*:\d*((\.|\:)\d*)*\]/g,''),//获取歌词
+					timeArr = arr[i].match(/\[\d*:\d*((\.|\:)\d*)*\]/g);//获取时间帧
+					if(timeArr){
+						//遍历时间帧
+						for(var j = 0, lenn = timeArr.length; j < lenn; j++){
+							var min = Math.floor(timeArr[j].match(/\[\d*/i).toString().slice(1)),//分
+							sec = Math.floor(timeArr[j].match(/\:\d*/i).toString().slice(1)),//秒
+							time = min * 60 + sec;//转换时间
+							player.lrc.options.data[time] = $.trim(text);//歌词对象
+				 		}
+				 	}
+				}
+	        	player.lrc.createUI(player.lrc.options.data, title, author);//创建歌词结构
+        	});
 		},
 		//创建歌词结构
-		createUI:function(data){
-			var i = 0, html = [];
+		createUI:function(data, title, author){
+			var i = 0;
         	for(var item in data){
-        		html[i]= '<p time='+ item +'>' + data[item] + '</p>';
+        		player.lrc.options.html[i]= '<p time='+ item +'>' + data[item] + '</p>';
         		i++;
         	}
-        	$lrc.html(html.join(' '));
+        	$lrc.html(player.lrc.options.html.join(' '));
+        	if (title)	$song_title.html(title);
+        	if (author)	$song_author.html(author);
+		}
+	};
+	
+	player.list = {
+		init: function(){
+			$.get('tracks.txt',function(data){
+				$("#play_list ul li").remove();
+				for(var i=0; i < data.songs.length; i++){
+					var sid = "song-item-" + new Date().getTime() + i;
+					data.songs[i].sid = sid;
+					songList.push(data.songs[i]);
+					$("#play_list ul").append(
+						$("#song_item").html().replace("{song}", data.songs[i].title)
+									.replace("{title}", data.songs[i].title + " - " + data.songs[i].artist + "《" + data.songs[i].album+ "》")
+									.replace("{sid}", sid)
+									.replace("{index}", i)
+					);
+				}
+				console.log(songList);
+				currentSong = Math.floor(Math.random() * (songList.length - 1));
+	      		$(".song_item").on("click", function(){
+	      			currentSong = $(this).attr('index');
+					player.changeSong();
+					player.setSkin();
+	        	});
+	        	player.init();
+        	}, "json");
+ 
 		}
 	};
 
@@ -289,12 +276,12 @@
 
 				player.volume.options.$dragging.css('top',offsetY);//设置音量游标位置
 				$volumeProgressCurrent.css('top',offsetY);//设置音量进度条位置
-				soundManager.setVolume(soundId,100 - (offsetY / player.volume.options.volHeight * 100));//设置音量
+				soundManager.setVolume(songList[currentSong].sid,100 - (offsetY / player.volume.options.volHeight * 100));//设置音量
 			}
 		},
 		up:function(){
 			if(player.volume.options.$dragging != null){
-				$volumePanel.fadeOut();
+				// $dragging = diffXY = dragXY = pageXY = offsetXY = scrollXY = containerXY = null;
 				player.volume.options.$dragging = null;
 
 				//注销事件，释放内存
@@ -350,23 +337,35 @@
 		}
     };
 
-	//播放暂停显隐
-	function togglePlay(flag){
-		//如果正在播放，则返回
-		if(!$pauseBtn.hasClass(player.css.vs) && flag){
-			return;
-		}
 
-		if(!$playBtn.hasClass(player.css.vs)){
-			$playBtn.toggleClass(player.css.vs);
-			$pauseBtn.toggleClass(player.css.vs);
-			player.sound.play();
-		}else{
-			$playBtn.toggleClass(player.css.vs);
-			$pauseBtn.toggleClass(player.css.vs);
-			player.sound.pause();
+	//显示隐藏播放暂停按钮
+	function showBtn(a,b){
+		a.hide();
+		b.show();
+		if(b.hasClass("pause-b")){
+			$(".play-btn").hide();
 		}
 	}
-
-	player.init();
-})(jQuery,window);
+	
+	function togglePanel(a, b){
+		if(!a.is(":visible"))	a.toggle();
+		else	a.toggle();
+		if(!b.is(":visible"))	b.toggle();
+		else	b.toggle();
+	}
+	player.list.init();
+	
+	
+	$(".cover img").hover(function(){
+		$(".play-btn").show();
+	},function(){
+		if($(".pause-b").is(":visible")){
+			$(".play-btn").hide();
+		}
+	});
+	
+	$(".play-btn").hover(function(){
+		$(".play-btn").show();
+	});
+	
+})(jQuery);
